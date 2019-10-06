e4516623aa3f5aae3e1d2b4b44ae908e0693366f
taki <takoreset@gmail.com>
2019-10-06 16:36:50 +0900

    first commit

---
+++ 2019_1005/a.py
@@ -0,0 +1,27 @@
+import sys
+
+
+def input():
+    return sys.stdin.readline().strip().decode('utf-8')
+
+
+S = input()
+K = int(input())
+t = ''
+
+for i in range(1, K + 1):
+    t = t + S
+
+t = list(t)
+replace_count = 0
+replace_word = '#'
+
+for i in range(1, len(t) + 1):
+    before_index = i - 1
+
+    if t[before_index] != replace_word and t[before_index] == t[i]:
+        replace_count += 1
+        t[i] = replace_word
+
+
+print(replace_count)
