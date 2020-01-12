325a4d4b0a9e953bf8414b00a3ba457ec4ec37a9
taki <takoreset@gmail.com>
2020-01-12 15:02:50 +0900

    abc 139 c fix

--- abc/139/c.py
+++ abc/139/c.py
@@ -1,19 +1,20 @@
-N = int(input())
-H = list(map(int, input().split()))
-
 
 def solve(N, H):
     ans = 0
     val = 0
 
-    for i in range(N):
-        if H[i] >= H[i+1]:
+    for i in range(N - 1):
+        if H[i] >= H[i + 1]:
             val += 1
         else:
             val = 0
+
         ans = max(val, ans)
 
     return ans
 
+
+N = int(input())
+H = list(map(int, input().split()))
 
 print(solve(N, H))
