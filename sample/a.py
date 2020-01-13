c3f3419bfb1f2c694051a3137511eeebe21ff123
taki <takoreset@gmail.com>
2020-01-13 11:31:46 +0900

    sample fix

--- sample/a.py
+++ sample/a.py
@@ -11,4 +11,9 @@ N = int(input())
 # A, B = map(int, input().split())
 # A = list(map(int, input().split()))
 
+# P = []
+# S = []
+# for i in range(N):
+#     P[i], S[i] = map(int, input().split())
+
 print(solve(N))
