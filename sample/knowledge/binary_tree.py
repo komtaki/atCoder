16115340c14c9d368b7f3ef97de6b843f578b894
taki <takoreset@gmail.com>
2020-01-19 23:14:53 +0900

    sample 二分探索追加

---
+++ sample/knowledge/binary_tree.py
@@ -0,0 +1,35 @@
+import math
+
+
+def check(A, B, X, mid):  # kが解として適しているかをチェックする
+    data_size = int(math.log10(mid) + 1)
+    return ((A * mid) + (B * data_size)) <= X
+
+
+# 解としてありえない値を入れる
+MAX = 1000000000
+
+
+# 二分探索で答えを探す
+def solve(A, B, X):
+    if (A + B) > X:
+        return 0
+    elif ((A * MAX) + (B * int(math.log10(MAX) + 1))) <= X:
+        return MAX
+
+    ok = 0
+    ng = MAX
+
+    while abs(ok - ng) > 1:
+        mid = (ng + ok) // 2
+        if check(A, B, X, mid):
+            ok = mid
+        else:
+            ng = mid
+
+    return ok
+
+
+A, B, X = map(int, input().split())
+
+print(solve(A, B, X))
