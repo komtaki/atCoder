9cf3762664d779ebc8de58d8ee86797abcda80a7
komiyama <komiyama@bengo4.com>
2020-01-21 15:53:57 +0900

    abc 151 e add

--- abc/151/e.py
+++ abc/151/e.py
@@ -1,26 +1,44 @@
-# import itertools
-# import math
-
 MOD = 10 ** 9 + 7
 
 
 def solve(N, K, A):
-    if K == 1:
-        return 0
+    A.sort()
+    ans = 0
 
-    sorted_a = A.sorted()
-    half_N = len(sorted_a) // 2
+    for i in range(1, N):
+        now = cmb(i, K - 1, MOD)
+        ans += A[i] * now
 
-    if half_N =< K:
-        for i in range(half_N):
-            
+    A.sort(reverse=True)
+    for i in range(1, N):
+        now = cmb(i, K - 1, MOD)
+        ans += A[i] * (-now)
 
-    return
+    return ans % MOD
 
 
 # N = int(input())
 # S = input()
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
+
+
+def cmb(n, r, p):
+    if (r < 0) or (n < r):
+        return 0
+    r = min(r, n - r)
+    return fact[n] * factinv[r] * factinv[n - r] % p
+
+
+p = 10 ** 9 + 7
+fact = [1, 1]
+factinv = [1, 1]
+inv = [0, 1]
+
+for i in range(2, N + 1):
+    fact.append((fact[-1] * i) % p)
+    inv.append((-inv[p % i] * (p // i)) % p)
+    factinv.append((factinv[-1] * inv[-1]) % p)
+
 
 print(solve(N, K, A))
