8ca83d38e687751d18c034b6c23a51189e12a0fd
taki <takoreset@gmail.com>
2020-02-08 16:28:37 +0900

    sample 更新

--- sample/contest/c.py
+++ sample/contest/c.py
@@ -5,20 +5,20 @@ import sys
 input = sys.stdin.readline
 
 
-def solve(N):
-    return
+def resolve():
+    # N = int(input())
+    # S = input()
+    # A, B = map(int, input().split())
+    # A = list(map(int, input().split()))
+
+    # P = []
+    # S = []
+    # for i in range(N):
+    #     pp, ss = map(int, input().split())
+    #     P.append(pp)
+    #     S.append(ss)
+
+    print()
 
 
-N = int(input())
-# S = input()
-# A, B = map(int, input().split())
-# A = list(map(int, input().split()))
-
-# P = []
-# S = []
-# for i in range(N):
-#     pp, ss = map(int, input().split())
-#     P.append(pp)
-#     S.append(ss)
-
-print(solve(N))
+resolve()
