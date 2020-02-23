d69066f71e1dc0feb0ea801acf4b91992238987f
taki <takoreset@gmail.com>
2020-02-23 11:41:43 +0900

    knowledge add

--- sample/knowledge/math.py
+++ sample/knowledge/math.py
@@ -11,3 +11,21 @@ def get_data_size(data):
         [int]: 桁数
     """
     return int(math.log10(data) + 1)
+
+
+def base_10_to_n(X, n):
+    """10進数をn進数に変換する
+
+    Args:
+        X (int): 10進数の値
+        n (int): n進数のn
+
+    Returns:
+        int: 10進数をn進数に変換した値
+    """
+    X_dumy = X
+    out = ''
+    while X_dumy > 0:
+        out = str(X_dumy % n) + out
+        X_dumy = int(X_dumy / n)
+    return out
